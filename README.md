0a3b023ea6f9efc53553271ff42a44efe2b1cd2b
LONZEE <128856310+LONZEE@users.noreply.github.com>
2024-05-05 09:48:06 -0500

    ran through an problem running hbs, fixed

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # MVC-TECH
+
+
+## Difficulties
+
+https://stackoverflow.com/questions/69959820/typeerror-exphbs-is-not-a-function
--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const app = express();
+
+// Handlebars Middleware
+app.engine('handlebars', exphbs.engine(
+    {
+       extname: '.hbs',
+    }
+));
+app.set('view engine', 'handlebars');
+
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
